1bcfddb99ddf012ac2166519670cf82c90aa372c
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-05-04 00:42:45 +0100

    Moved file to src folder and  refactoring require_once directory

---
+++ src/init.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Parses and verifies the doc comments for files.
+ *
+ * PHP version >=5
+ *
+ * @category  PHP
+ * @package   PagaMerchant
+ * @author    PagaDevComm <devcomm@paga.com>
+ * @copyright 2020 Pagatech Financials
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      https://packagist.org/packages/paga/paga-merchant
+ */
+require_once __DIR__ .'/../vendor/autoload.php'
+?>
